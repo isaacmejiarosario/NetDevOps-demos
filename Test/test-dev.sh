3fe93060d67ece54e671ffce86b8f7d4c53b57fd
root <root@localhost.localdomain>
2021-09-29 11:26:23 -0700

    mpls l3vpn demo  update CR#5.14

--- Test/test-dev.sh
+++ Test/test-dev.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd Test/
-python3 mpls_l3vpn_easypy_test.py
+cd Test && python3 mpls_l3vpn_easypy_test.py
--- mpls_l3vpn_easypy_test.py
+++ mpls_l3vpn_easypy_test.py
@@ -4,7 +4,8 @@ from pyats.easypy import run
 ####I.MEJIA_MPLS_L3VPN_NetDevOps_CI/CI_PIPELINE_LAB#####
 def main(runtime):
     runtime.job.name = 'mpls_l3vpn_easypy_test'
-    script_path = f"{os.path.dirname(os.path.abspath(__file__))}/Test"
+    #script_path = f"{os.path.dirname(os.path.abspath(__file__))}/Test"
+    script_path = os.path.dirname(os.path.abspath(__file__))
     ospf_test = os.path.join(script_path, 'ospf_test_all_vrfs.py')
     ldp_test = os.path.join(script_path, 'ldp_test.py')
     bgp_test = os.path.join(script_path, 'bgp_test_all_vrfs.py')
